10ca70ae548e3d5dd8b91c3486cd067a3190ef96
Megan Kossa <dragon.flame.meganite+github@gmail.com>
2024-07-21 16:03:02 -0500

    Playing around with bg and text

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -5,6 +5,7 @@ export class Game extends Scene
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     msg_text : Phaser.GameObjects.Text;
+    msg_text2: Phaser.GameObjects.Text;
 
     constructor ()
     {
@@ -19,12 +20,19 @@ export class Game extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+        this.msg_text = this.add.text(512, 384, 'This is totally going to be a game some day!\n' +
+            'Pet catching game, the thingy!', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         });
         this.msg_text.setOrigin(0.5);
+        this.msg_text2 = this.add.text(512, screen.availHeight/2 + 200, 'Make it Awesome ✔️', {
+            fontFamily: 'Arial Black', fontSize: 20, color: '#CCCCCC',
+            stroke: '#000000', strokeThickness: 4,
+            align: 'center'
+        } );
+        this.msg_text2.setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
 
--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -19,7 +19,7 @@ export class GameOver extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.gameover_text = this.add.text(512, 384, 'Game Over', {
+        this.gameover_text = this.add.text(512, 384, 'That\'s all for now!\nSee y\'all later!', {
             fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
